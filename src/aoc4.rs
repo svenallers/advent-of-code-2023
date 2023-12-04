ad9771af671e6edb6074b72e50320fbcdba97c6c
Sven Allers <sven.allers@gmx.de>
2023-12-04 16:11:16 +0100

    Day 4: Scratchcards

---
+++ src/aoc4.rs
@@ -0,0 +1,88 @@
+use once_cell::sync::Lazy;
+use regex::{Captures, Regex};
+
+use crate::fs_util::read_or_panic;
+
+#[derive(Clone)]
+struct Card {
+    number: usize,
+    winning_numbers: Vec<usize>,
+    own_numbers: Vec<usize>,
+}
+
+impl Card {
+    fn points(&self) -> usize {
+        let matches = self.matches();
+        return if matches > 0 {
+            2usize.pow(matches as u32 - 1)
+        } else {
+            0
+        }
+    }
+
+    fn matches(&self) -> usize {
+        self.winning_numbers.iter().filter(|number| self.own_numbers.contains(number)).collect::<Vec<_>>().len()
+    }
+}
+
+pub fn aoc_4_1() -> usize {
+    return parse_and_sum_up_points("res/aoc4.txt");
+}
+
+pub fn aoc_4_2() -> usize {
+    return parse_and_count_all_cards_including_won_ones("res/aoc4.txt");
+}
+
+fn parse_and_sum_up_points(input_file: &str) -> usize {
+    let input_data = read_or_panic(input_file);
+    return parse_input_data(&input_data).iter().map(|card| card.points()).sum();
+}
+
+fn parse_and_count_all_cards_including_won_ones(input_file: &str) -> usize {
+    let input_data = read_or_panic(input_file);
+    let original_cards = parse_input_data(&input_data);
+    let mut all_cards: Vec<Card> = original_cards.iter().map(|card| card.clone()).collect();
+    let mut i = 0;
+    while i < all_cards.len() {
+        let card = all_cards[i].clone();
+        let matches = card.matches();
+        for n in 0..matches {
+            if let Some(won_card) = original_cards.get(card.number + n) {
+                all_cards.insert(i + 1, won_card.clone());
+            }
+        }
+        i += 1;
+    }
+    return all_cards.len();
+}
+
+static CARD_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"Card\s+(?<number>\d+):\s*(?<winning_numbers>(\d+\s*)*)\|\s*(?<own_numbers>(\d+\s*)*)(\n|$)").unwrap());
+
+fn parse_input_data(input_data: &str) -> Vec<Card> {
+    return CARD_REGEX.captures_iter(input_data).map(|matched_card| {
+        Card {
+            number: matched_card.name("number").unwrap().as_str().parse().unwrap(),
+            winning_numbers: parse_numbers(&matched_card, "winning_numbers"),
+            own_numbers: parse_numbers(&matched_card, "own_numbers"),
+        }
+    }).collect();
+}
+
+fn parse_numbers(capture: &Captures, group_name: &str) -> Vec<usize>{
+    capture.name(group_name).unwrap().as_str().split(" ").filter(|num| !num.is_empty()).map(|num| num.parse().unwrap()).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::aoc4::{parse_and_count_all_cards_including_won_ones, parse_and_sum_up_points};
+
+    #[test]
+    fn aoc_4_1() {
+        assert_eq!(parse_and_sum_up_points( "res/aoc4-example.txt"), 14)
+    }
+
+    #[test]
+    fn aoc_4_2() {
+        assert_eq!(parse_and_count_all_cards_including_won_ones( "res/aoc4-example.txt"), 31)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use crate::aoc1::{aoc_1_1, aoc_1_2};
 use crate::aoc2::{aoc_2_1, aoc_2_2};
 use crate::aoc3::{aoc_3_1, aoc_3_2};
+use crate::aoc4::{aoc_4_1, aoc_4_2};
 
 mod aoc1;
 mod aoc2;
 mod aoc3;
+mod aoc4;
 mod fs_util;
 
 fn main() {
@@ -14,4 +16,6 @@ fn main() {
     println!("AoC 2.2: {}", aoc_2_2());
     println!("AoC 3.1: {}", aoc_3_1());
     println!("AoC 3.2: {}", aoc_3_2());
+    println!("AoC 4.1: {}", aoc_4_1());
+    println!("AoC 4.2: {}", aoc_4_2());
 }
